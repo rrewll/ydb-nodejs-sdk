370d799ce56af4826e1d788c4ff0ecb11a72aa81
rorewillo <rorewillo@yandex-team.ru>
2021-09-07 16:43:09 +0300

    skip connection test + delete docker initialization

--- src/__tests__/connection.test.ts
+++ src/__tests__/connection.test.ts
@@ -2,7 +2,7 @@ import Driver from '../driver';
 import {AnonymousAuthService} from '../credentials';
 
 describe('Connection', () => {
-    it('Test connection', async () => {
+    it.skip('Test connection', async () => {
         const driver = new Driver('grpc://localhost:2135', 'local', new AnonymousAuthService());
         await driver.ready(10000);
         await driver.tableClient.withSession(async (session) => {
