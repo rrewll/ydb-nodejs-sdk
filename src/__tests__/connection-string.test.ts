8bc0ee4848b548f381091150d2d9537901f824ab
rorewillo <rorewillo@yandex-team.ru>
2021-09-07 14:28:38 +0300

    more fixes

--- src/__tests__/connection-string.test.ts
+++ src/__tests__/connection-string.test.ts
@@ -1,23 +1,33 @@
-import {parseConnectionString} from "../parse-connection-string";
+import {parseConnectionString} from '../parse-connection-string';
 
 describe('Connection string', () => {
-    it('Test parseConnectionString', () => {
+    it('test parseConnectionString', () => {
         const tests = [
+            {
+                connectionString: 'ydb-ru.yandex.net:2135/?database=/ru/home/service/db',
+                endpoint: 'grpcs://ydb-ru.yandex.net:2135',
+                database: '/ru/home/service/db',
+            },
             {
                 connectionString: 'grpc://ydb-ru.yandex.net:2135/?database=/ru/home/service/db',
                 endpoint: 'grpc://ydb-ru.yandex.net:2135',
                 database: '/ru/home/service/db',
             },
             {
-                connectionString: "grpcs://ydb.serverless.yandexcloud.net:2135/?database=/ru-central1/b1g8skpblkos03malf3s/etn02qso4v3isjb00te1",
+                connectionString: 'grpcs://ydb-ru.yandex.net:2135/?database=/ru/home/service/db',
+                endpoint: 'grpcs://ydb-ru.yandex.net:2135',
+                database: '/ru/home/service/db',
+            },
+            {
+                connectionString: "grpcs://ydb.serverless.yandexcloud.net:2135/?database=/ru-central1/b1g8skpblkos03malf3s/etn03nohfn502cpa0cfe",
                 endpoint: "grpcs://ydb.serverless.yandexcloud.net:2135",
-                database: "/ru-central1/b1g8skpblkos03malf3s/etn02qso4v3isjb00te1"
+                database: "/ru-central1/b1g8skpblkos03malf3s/etn03nohfn502cpa0cfe"
             }
         ];
 
         tests.forEach((test) => {
             const parsedString = parseConnectionString(test.connectionString);
-            expect(parsedString).toEqual(test);
+            expect(parsedString).toEqual({endpoint: test.endpoint, database: test.database});
         });
     });
 });
