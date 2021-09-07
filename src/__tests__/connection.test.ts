a52c55eef4e8d9f798101dda6570136707c7961d
rorewillo <rorewillo@yandex-team.ru>
2021-09-07 18:19:02 +0300

    use testcontainers to run docker

--- src/__tests__/connection.test.ts
+++ src/__tests__/connection.test.ts
@@ -1,9 +1,24 @@
 import Driver from '../driver';
 import {AnonymousAuthService} from '../credentials';
+import {GenericContainer, StartedTestContainer} from "testcontainers";
+
+
 
 describe('Connection', () => {
+    let startedContainer: StartedTestContainer;
+
+    beforeAll(async () => {
+        startedContainer = await new GenericContainer('cr.yandex/yc/yandex-docker-local-ydb:latest')
+            .withExposedPorts(2135)
+            .start();
+    });
+
+    afterAll(async () => {
+        await startedContainer.stop();
+    })
+
     it('Test connection', async () => {
-        const driver = new Driver('grpc://localhost:2135', 'local', new AnonymousAuthService());
+        const driver = new Driver('grpc://' + startedContainer.getHost() + ':2135', 'local', new AnonymousAuthService());
         await driver.ready(10000);
         await driver.tableClient.withSession(async (session) => {
             await session.executeQuery('SELECT 1');
